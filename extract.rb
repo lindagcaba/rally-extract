2d4a4be815d07d50ee8e523ccc183a321fa3b7b1
Igor von Nyssen <igor.vonnyssen@am.sony.com>
2012-06-26 17:55:36 -0700

    retrieving in_progress_date from revision history as it can change

--- extract.rb
+++ extract.rb
@@ -26,7 +26,7 @@ def process_story(us, rows)
 
   execution_time = (DateTime.parse(us.accepted_date) - DateTime.parse(us.in_progress_date)).to_f
 
-  rows << [us.formatted_i_d, us.name, us.project, us.owner, us.in_progress_date, us.accepted_date, execution_time, us.defects ? us.defects.size : 0, us.tasks ? us.tasks.size : 0]
+  rows << [us.formatted_i_d, us.name, us.project, us.owner, us.in_progress_date, us.accepted_date, execution_time, us.defects ? us.defects.size : 0, us.tasks ? us.tasks.size : 0, us.plan_estimate ? us.plan_estimate : 0, us.package, us.task_estimate_total]
 
   write_description_and_tasks(story_description_folder, us)
   write_history(history_folder, us)
@@ -43,12 +43,12 @@ params = {
 
 rally = RallyRestAPI.new(params)
 
-project = rally.find(:project) {
-  equal :name, 'NPMT Scrum Team'
-}.first
+#project = rally.find(:project) {
+#  equal :name, 'NPMT Scrum Team'
+#}.first
 
 results = rally.find(:hierarchicalrequirement) {
-  equal :project, project
+#  equal :project, project
   _or_ {
     equal :schedule_state, 'Signed-Off'
     equal :schedule_state, 'Accepted'
@@ -58,12 +58,25 @@ results = rally.find(:hierarchicalrequirement) {
 
 rows = []
 results.each do |us|
-  if us.in_progress_date && us.accepted_date 
-    process_story(us, rows)
+  in_progress_date = ""
+  us.revision_history.revisions.each do |rev|
+    in_progress_date = rev.creation_date if /.*IN PROGRESS DATE added.*/.match(rev.description)
+  end
+  
+  in_progress_date = us.in_progress_date if in_progress_date.empty?
+  
+  if in_progress_date && us.accepted_date
+    puts "------------Story:" + us.formatted_i_d
+    puts "------------DATE: " + in_progress_date
+    execution_time = (DateTime.parse(us.accepted_date) - DateTime.parse(in_progress_date)).to_f
+    if (execution_time > 0) && (us.children.nil?)
+      us.in_progress_date = in_progress_date
+      process_story(us, rows)
+    end
   end
 end
 
-header = ['id', 'name', 'project', 'owner', 'start', 'end', 'execution_in_days', 'defects', 'tasks']
+header = ['id', 'name', 'project', 'owner', 'start', 'end', 'execution_in_days', 'defects', 'tasks', 'plan_estimate', 'package', 'task_estimate_total']
 CSV.open("out.csv", "w") do |csv|
   csv << header
   rows.each {|row| csv << row}
